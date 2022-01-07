efb0d8683239ad2879d44b4e8ed2ef3f35b98b2d
Michael <hyper00border@gmail.com>
2022-01-07 12:41:23 +0300

    Fixed search

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ReactSearchBox from 'react-search-box';
 import { Cabinet } from './Cabinet';
 import { searchGameUrl } from './constants';
@@ -7,25 +7,30 @@ import { LoginRegister } from './LoginRegister';
 import { MainView } from './MainView';
 import './styles/Header.css';
 import { GameRecord } from './utils';
+import Select, { components, InputActionMeta, SingleValue } from "react-select";
+
 export function Header({ setCurrentView }: { setCurrentView: Function }) {
 
-  const [searchData, setSearchData]: [GameRecord[], Function] = useState([]);
-  let [searchGamesNames, setSearchGamesNames]: [{key: string, value: string }[], Function]
+  // const [searchData, setSearchData]: [GameRecord[], Function] = useState([]);
+  let [searchGamesNames, setSearchGamesNames]: [{ key: string, value: string,label:string }[], Function]
     = useState([]);
   function getSearchData(namePart: string) {
     let url = `${searchGameUrl}?tags=&maxPrice=999999&minPrice=-1&minMark=0&namePart=${namePart}`;
     fetch(url).then(data => data.json()).then(json => {
-      setSearchData({ ...json });
-      searchGamesNames=[];
+      // setSearchData({ ...json });
+      searchGamesNames = [];
       if (json) {
         json.forEach((val: GameRecord) => {
-          searchGamesNames.push({ key: val.name, value: val.name });
+          searchGamesNames.push({ key: val.name, value: val.name,label:val.name });
         });
       }
       console.log('Data for search', searchGamesNames);
       setSearchGamesNames([...searchGamesNames]);
     });
   }
+  useEffect(() => {
+    getSearchData('');
+  }, [])
   function logout() {
     localStorage.removeItem('password');
     localStorage.removeItem('name');
@@ -38,15 +43,16 @@ export function Header({ setCurrentView }: { setCurrentView: Function }) {
       setCurrentView(<MainView setCurrentView={setCurrentView} />)
     }}>Main</button>
     <div className='header-search-field'>
-      <ReactSearchBox
+      <Select
         placeholder={'Search'}
-        data={searchGamesNames}
-        onChange={
-          (val: string) => {
-            getSearchData(val);
+        options={searchGamesNames}
+        onInputChange={
+          (newValue:string,actionMeta:InputActionMeta) => {
+            if (newValue) {
+              getSearchData(newValue);
+            }
           }
         }
-        onSelect={() => { }}
       />
     </div>
     <button className='header-btn'>Wishlist</button>
